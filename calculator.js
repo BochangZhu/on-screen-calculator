07f0060d53a5b188459791a4dfec3a11de652b9a
BochangZhu <160828488+BochangZhu@users.noreply.github.com>
2026-05-02 15:44:53 -0700

    fix dot allowed to enter multiple times & errors not display correctly

--- calculator.js
+++ calculator.js
@@ -3,10 +3,17 @@ function round3dp(num){
     return Math.round(num * 1000)/ 1000;
 }
 
+
+// Create node variables
+const numbers = document.querySelector(".numbers");
+const operators = document.querySelector(".operators");
+const display = document.querySelector(".display");
+
+
 let add = (a,b) => round3dp(+a + +b);
 let substract = (a,b) => round3dp(a - b);
 let multiply = (a,b) => round3dp(a * b);
-let divide = (a,b) => b ? round3dp(a / b) : "Division By Zero";
+let divide = (a,b) => +b ? round3dp(a / b) : "Division By Zero";
 let power = (a,b) => round3dp(a ** b);
 
 // Helper func to perform different calculations
@@ -20,7 +27,7 @@ let operate = (m,a,b) => {
             return multiply(a,b);
         case "/":
             if (typeof divide(a,b) == 'string'){
-                display.textContent = "DIVISION BY ZERO"
+                display.textContent = "DIVISION BY ZERO";
                 clearBuffer();
                 return "0";
             };
@@ -29,10 +36,9 @@ let operate = (m,a,b) => {
             return power(a,b);
         default:
             // invalid calculation would to led restart of board
-            display.textContent = "SYNTAX ERROR"
+            display.textContent = "SYNTAX ERROR";
             clearBuffer();
             return "0";
-
     }
 };
 
@@ -48,10 +54,6 @@ let override = 1;
 // Init list
 let opList = [{text : "+", func : add}, {text : "-", func : substract}, {text: "*", func: multiply }, {text:"/", func:divide}, {text:"**", func:power}];
 
-// Create node variables
-const numbers = document.querySelector(".numbers");
-const operators = document.querySelector(".operators");
-const display = document.querySelector(".display");
 
 // Helper func to display screen
 function updateDisplay(){
@@ -62,7 +64,7 @@ function updateDisplay(){
 function clearBuffer(){
     num1Turn = 1;
     override = 1;
-
+    dotEntered = 0;
     num1 = "0";
     operator = "";
     num2 = "";
@@ -98,32 +100,36 @@ for (let num = 0; num <= 9; num ++){
     numbers.appendChild(curr_num);
 }
 
+let dotEntered = 0;
 let dotSign = document.createElement("div");
 dotSign.textContent = ".";
 dotSign.classList.add("number", "border");
 dotSign.addEventListener("click", e =>{
-    if (num1Turn){
-        if (override){
-            num1 = e.target.textContent;
-            override = 0;
-            updateDisplay();
+    if (!dotEntered){
+        if (num1Turn){
+            if (override){
+                num1 = e.target.textContent;
+                dotEntered = 1;
+                override = 0;
+                updateDisplay();
+            }
+            else{
+                dotEntered = 1;
+                num1 += e.target.textContent;
+                updateDisplay();
+            }
         }
         else{
-            num1 += e.target.textContent;
+            dotEntered = 1;
+            num2 += e.target.textContent;
             updateDisplay();
         }
     }
-    else{
-        num2 += e.target.textContent;
-        updateDisplay();
-    }
-    
 });
 
 let clear = document.createElement("div");
 clear.textContent = "CE";
-clear.classList.add("number", "border");
-clear.style["background-color"] = "#ffb5b5";
+clear.classList.add("number", "border", "CE");
 clear.addEventListener("click", ()=> {
     clearBuffer();
     updateDisplay();
@@ -141,10 +147,12 @@ opList.forEach(op=>{
         // 112 (current state, enter or leave blank for num1)
         if (num2 == ""){
             num1Turn = 0; // goes to num2's turn
+            dotEntered = 0; // refresh dot entered counter
             operator = op.text;
             updateDisplay();
         }
         else{
+            dotEntered = 0;
             num1 = operate(operator, num1, num2);
             operator = op.text;
             num2 = "";
@@ -160,11 +168,14 @@ const eqSign = document.createElement("div");
 eqSign.textContent = "=";
 eqSign.addEventListener("click", () => {
     num1 = operate(operator, num1, num2);
-    operator = "";
-    num2 = "";
-    override = 1;
-    num1Turn = 1;
-    updateDisplay();
+    if (display.textContent != "DIVISION BY ZERO" && display.textContent != "SYNTAX ERROR"){
+        operator = "";
+        num2 = "";
+        dotEntered = 0;
+        override = 1;
+        num1Turn = 1;
+        updateDisplay();
+    }
 })
 eqSign.classList.add("border", "eqSign");
 operators.appendChild(eqSign);
