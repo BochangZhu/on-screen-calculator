edffb991a866ffa0a175957ed557f86daa783c0a
BochangZhu <160828488+BochangZhu@users.noreply.github.com>
2026-05-02 15:07:25 -0700

    add missing 0 & dot & CE keys & fix invalid calculation edge cases

--- calculator.js
+++ calculator.js
@@ -1,9 +1,15 @@
-let add = (a,b) => +a + +b;
-let substract = (a,b) => a - b;
-let multiply = (a,b) => a * b;
-let divide = (a,b) => a / b;
-let power = (a,b) => a ** b;
+// basic calculator functions
+function round3dp(num){
+    return Math.round(num * 1000)/ 1000;
+}
 
+let add = (a,b) => round3dp(+a + +b);
+let substract = (a,b) => round3dp(a - b);
+let multiply = (a,b) => round3dp(a * b);
+let divide = (a,b) => b ? round3dp(a / b) : "Division By Zero";
+let power = (a,b) => round3dp(a ** b);
+
+// Helper func to perform different calculations
 let operate = (m,a,b) => {
     switch (m){
         case "+":
@@ -13,38 +19,137 @@ let operate = (m,a,b) => {
         case "*":
             return multiply(a,b);
         case "/":
+            if (typeof divide(a,b) == 'string'){
+                display.textContent = "DIVISION BY ZERO"
+                clearBuffer();
+                return "0";
+            };
             return divide(a,b);
         case "**":
             return power(a,b);
+        default:
+            // invalid calculation would to led restart of board
+            display.textContent = "SYNTAX ERROR"
+            clearBuffer();
+            return "0";
+
     }
 };
 
-let num1 = "";
+// global variables
+let num1 = "0";
 let operator = "";
 let num2 = "";
 
+// special keywords
+let num1Turn = 1;
+let override = 1;
+
+// Init list
 let opList = [{text : "+", func : add}, {text : "-", func : substract}, {text: "*", func: multiply }, {text:"/", func:divide}, {text:"**", func:power}];
+
+// Create node variables
 const numbers = document.querySelector(".numbers");
 const operators = document.querySelector(".operators");
+const display = document.querySelector(".display");
 
-for (let num = 1; num <= 9; num ++){
+// Helper func to display screen
+function updateDisplay(){
+    display.textContent = num1 + operator + num2;
+}
+
+// helper func to clear buffer
+function clearBuffer(){
+    num1Turn = 1;
+    override = 1;
+
+    num1 = "0";
+    operator = "";
+    num2 = "";
+}
+
+// Create the nine numbers
+for (let num = 0; num <= 9; num ++){
     const curr_num = document.createElement("div");
+    // store number as textcontent
     curr_num.textContent = num;
+    // add proper classlists
     curr_num.classList.add(`num${num}`, "number", "border");
     curr_num.addEventListener("click", e =>{
-        num1 += e.target.textContent;
-        alert(num1);
+        // update when its num1's turn override after 
+        if (num1Turn){
+            if (override){
+                num1 = e.target.textContent;
+                override = 0;
+                updateDisplay();
+            }
+            else{
+                num1 += e.target.textContent;
+                updateDisplay();
+            }
+        }
+        else{
+            num2 += e.target.textContent;
+            updateDisplay();
+        }
+        
     });
     
     numbers.appendChild(curr_num);
 }
 
+let dotSign = document.createElement("div");
+dotSign.textContent = ".";
+dotSign.classList.add("number", "border");
+dotSign.addEventListener("click", e =>{
+    if (num1Turn){
+        if (override){
+            num1 = e.target.textContent;
+            override = 0;
+            updateDisplay();
+        }
+        else{
+            num1 += e.target.textContent;
+            updateDisplay();
+        }
+    }
+    else{
+        num2 += e.target.textContent;
+        updateDisplay();
+    }
+    
+});
+
+let clear = document.createElement("div");
+clear.textContent = "CE";
+clear.classList.add("number", "border");
+clear.style["background-color"] = "#ffb5b5";
+clear.addEventListener("click", ()=> {
+    clearBuffer();
+    updateDisplay();
+})
+
+numbers.appendChild(dotSign);
+numbers.appendChild(clear);
+
+
 opList.forEach(op=>{
     const currOp = document.createElement("div");
+    // assign correct operators
     currOp.textContent = op.text;
     currOp.addEventListener("click", (e)=>{
-        operator = op.text;
-        alert(operator);
+        // 112 (current state, enter or leave blank for num1)
+        if (num2 == ""){
+            num1Turn = 0; // goes to num2's turn
+            operator = op.text;
+            updateDisplay();
+        }
+        else{
+            num1 = operate(operator, num1, num2);
+            operator = op.text;
+            num2 = "";
+            updateDisplay();
+        }
     })
     currOp.classList.add("operator", "border");
     operators.appendChild(currOp);
@@ -53,7 +158,13 @@ opList.forEach(op=>{
 
 const eqSign = document.createElement("div");
 eqSign.textContent = "=";
-eqSign.addEventListener("click", e=>{   
+eqSign.addEventListener("click", () => {
+    num1 = operate(operator, num1, num2);
+    operator = "";
+    num2 = "";
+    override = 1;
+    num1Turn = 1;
+    updateDisplay();
 })
 eqSign.classList.add("border", "eqSign");
 operators.appendChild(eqSign);
